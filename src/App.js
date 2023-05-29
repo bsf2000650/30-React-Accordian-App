72001932cb12beefc5e01c9df3b83fa36f65b496
Muhammad Asad <114976729+bsf2000650@users.noreply.github.com>
2023-05-29 18:26:19 +0500

    Accordian App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { question } from './question';
+import MyAccordion from './MyAccordion'
 
 function App() {
+  const [data, setData] = useState(question);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section className='main-div'>
+    <h1>React Interview Questions</h1>
+        {
+            data.map((curElem) => {
+              const {id}  = curElem;
+              return <MyAccordion key={id} {...curElem} />
+            })
+        }
+        </section>
+    </>
   );
 }
 
---
+++ src/question.jsx
@@ -0,0 +1,32 @@
+export const question = [
+    {
+        id : 1,
+        question : 'What is the vitual DOM?',
+        answer : 'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree strucure.'
+    },
+    {
+        id : 2,
+        question : 'What is the vitual DOM?',
+        answer : 'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree strucure.'
+    },
+    {
+        id : 3,
+        question : 'What is the vitual DOM?',
+        answer : 'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree strucure.'
+    },
+    {
+        id : 4,
+        question : 'What is the vitual DOM?',
+        answer : 'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree strucure.'
+    },
+    {
+        id : 5,
+        question : 'What is the vitual DOM?',
+        answer : 'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree strucure.'
+    },
+    {
+        id : 6,
+        question : 'What is the vitual DOM?',
+        answer : 'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree strucure.'
+    }
+]
